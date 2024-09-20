718c46cc50d2523a534d76b0972bccf5ac8d286a
jommka <pretty.pelmeshka@mail.ru>
2024-09-20 17:24:13 +0300

    Modal widow

--- car-cloud-control/src/components/UI/Modal/Modal.js
+++ car-cloud-control/src/components/UI/Modal/Modal.js
@@ -2,24 +2,52 @@ import React from 'react';
 import add from "../Header/icon/add.svg";
 import './style/Modal.css'
 import output from "../../icon/output.png";
+import usePasswordToggle from "../../../hooks/usePasswordToggle";
 
 const Modal = ({ show, onCloseButtonClick }) => {
+    const [PasswordInputType, ToggleIcon] = usePasswordToggle()
+
     if (!show) {
         return null;
     }
 
     return (
         <div className="modal-wrapper">
-            <div style={{position: "relative", width: "auto", left: '50%', transform: 'translateX(-50%)'}}>
+            <div style={{display: "flex", alignItems: "center", justifyContent: 'center', height: '100%'}}>
                 <div className="modal-content">
-                    <h1 style={{fontSize: '32px', fontWeight: '400'}}>Добавление устройства</h1>
+                    <h1 style={{fontSize: '30px', fontWeight: '400', textAlign: "center", marginBottom: '30px'}}>Добавление устройства</h1>
                     <div className='modal-info'>
-                        <img src={output} alt="device" style={{width: "100px", height: '100px'}}/>
+                        <img src={output} alt="device" style={{width: "120px", height: '120px'}}/>
                         <div className='line'></div>
-                        <div>
-                            <button className="button_add"><img src={add} alt=""/>Добавить</button>
-                            <button onClick={onCloseButtonClick}>Отмена</button>
-                        </div>
+                        <form>
+                            <div className="block-input">
+                                <input type="text" required spellCheck="false"></input>
+                                <label>Логин</label>
+                            </div>
+                            {/*<div className="block-input">*/}
+                            {/*    <input type="password" required spellCheck="false"></input>*/}
+                            {/*    <label>Пароль</label>*/}
+                            {/*</div>*/}
+                            <div className="block-input">
+                                <input type={PasswordInputType} required spellCheck="false"></input>
+                                <label>Пароль</label>
+                                <span>
+                                    {ToggleIcon}
+                                </span>
+                            </div>
+                        </form>
+                    </div>
+
+                    <div style={{
+                        display: "flex",
+                        alignItems: "center",
+                        justifyContent: "center",
+                        marginLeft: "auto",
+                        marginRight: '29px',
+                        gap: "40px",
+                    }}>
+                        <button className="button_add"><img src={add} alt=""/>Добавить</button>
+                        <button onClick={onCloseButtonClick} className="cancel">Отмена</button>
                     </div>
                 </div>
             </div>
